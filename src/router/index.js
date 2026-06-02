c99302d0a65a669a62c7175120f6de04ee560500
vinicius borba <viniboal28@gmail.com>
2026-06-02 15:50:37 -0300

    feat add citizens to router

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,8 @@ import CompanyList from '../views/companies/CompanyList.vue'
 import CompanyForm from '../views/companies/CompanyForm.vue'
 import LocationList from '../views/locations/LocationList.vue'
 import LocationForm from '../views/locations/LocationForm.vue'
+import CitizenList from '../views/citizens/CitizenList.vue'
+import CitizenForm from '../views/citizens/CitizenForm.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +17,9 @@ const router = createRouter({
     { path: '/locations', component: LocationList },
     { path: '/locations/new', component: LocationForm },
     { path: '/locations/:id/edit', component: LocationForm },
+    { path: '/citizens', component: CitizenList },
+    { path: '/citizens/new', component: CitizenForm },
+    { path: '/citizens/:id/edit', component: CitizenForm },
   ],
 })
 
